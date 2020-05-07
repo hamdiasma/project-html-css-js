922652364e606575f70ff9765f0e022c304e1976
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-05-07 15:28:58 +0100

    selectllianding-page.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// select landing page element
+let landingPage = document.getElementById("landing-page");
+// array of images
+let arrayImages = ["06.jpg", "07.jpg", "08.jpg"];
+// change bckground url
+landingPage.style.backgroundImage = 'url("../assets/07.jpg")';
