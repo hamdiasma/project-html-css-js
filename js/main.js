ff038931a8f3945c68e7ac1e8662f0b8e7613396
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-05-07 17:38:14 +0100

    add setting-box

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
+// select fa-gerar spin
+let myGear = document.getElementById("fa-gear");
+myGear.onclick = function () {
+  //toogle on self
+  this.classList.toggle("fa-spin");
+  document.getElementById("setting-box").classList.toggle("open");
+};
+
+// switch colors
+const colorsLi = document.querySelectorAll(".color-list li");
+
+colorsLi.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    // set color in root
+
+    document.documentElement.style.setProperty(
+      "--main--color",
+      e.target.dataset.color
+    );
+  });
+});
+// console.log(colorsLi)
+
 // select landing page element
 let landingPage = document.getElementById("landing-page");
 // array of images
 let arrayImages = ["06.jpg", "07.jpg", "08.jpg"];
-// change bckground url
-landingPage.style.backgroundImage = 'url("../assets/07.jpg")';
+
+setInterval(() => {
+  // get random number of arrayImages
+  let myNumb = Math.floor(Math.random() * arrayImages.length);
+  // change bckground url
+  //   landingPage.style.backgroundImage = `url("../assets/${arrayImages[myNumb]}")`;
+}, 1000);
