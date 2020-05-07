aad3e8145f63b820763942eac3126bdda197689f
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-05-07 21:04:19 +0100

    toogle setting-box

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-
-
-
-
-
-
+//onclick toggle setting-box 
+let gearSeting = document.getElementById("fa-gear");
+gearSeting.onclick = function () {
+  this.classList.toggle("fa-spin");
+  document.getElementById("setting-box").classList.toggle("open")
+};
 
 // random landing back ground
 let myImages = document.getElementById("landing-page");
@@ -12,7 +12,5 @@ const imagesArray = ["06.jpg", "07.jpg", "08.jpg"];
 
 setInterval(() => {
   let randomImager = Math.floor(Math.random() * imagesArray.length);
-myImages.style.backgroundImage=`url('assets/${imagesArray[randomImager]}')`
+  myImages.style.backgroundImage = `url('assets/${imagesArray[randomImager]}')`;
 }, 1000);
-
-
