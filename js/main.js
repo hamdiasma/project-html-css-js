0bd429fa35335440309385e26d5ff039d7c8c7ba
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-05-07 21:24:12 +0100

    switch color

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-//onclick toggle setting-box 
+//onclick toggle setting-box
 let gearSeting = document.getElementById("fa-gear");
 gearSeting.onclick = function () {
   this.classList.toggle("fa-spin");
-  document.getElementById("setting-box").classList.toggle("open")
+  document.getElementById("setting-box").classList.toggle("open");
 };
+
+// switch color
+
+const clorLi = document.querySelectorAll(".color-list li");
+console.log(clorLi);
+clorLi.forEach((li) => {
+  li.addEventListener("click", (e) => {
+    // set color on root --main--color
+   document.documentElement.style.setProperty("--main--color",e.target.dataset.color)
+  });
+});
 
 // random landing back ground
 let myImages = document.getElementById("landing-page");
@@ -13,4 +24,4 @@ const imagesArray = ["06.jpg", "07.jpg", "08.jpg"];
 setInterval(() => {
   let randomImager = Math.floor(Math.random() * imagesArray.length);
   myImages.style.backgroundImage = `url('assets/${imagesArray[randomImager]}')`;
-}, 1000);
+}, 10000);
