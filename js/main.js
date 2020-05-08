e60d194085aa3dc78f716bfe304f65bba58a5cd2
hamdiasma <hamdi.babdelhafidh@gmail.com>
2020-05-08 13:57:57 +0100

    setInterval landing-page images

--- js/main.js
+++ js/main.js
@@ -1,78 +1,20 @@
-// chek active linke
-// let myPage = localStorage.getItem("my-page");
+// select landing page
 
-// on  chek the local storage color
+let landngPage = document.getElementById("landing-page");
 
-let storeColor = localStorage.getItem("saving-color");
+// get array of images
 
-if (storeColor !== null) {
-  document.documentElement.style.setProperty("--main--color", storeColor);
-
-  // chek for active class
-  // remove all active class from all children
-  document.querySelectorAll(".color-list li").forEach((element) => {
-    element.classList.remove("active");
-    if (element.dataset.color === storeColor) {
-      //add active classe on elemnet  data-color === local storage
-      element.classList.add("active");
-    }
-  });
-}
-
-// // page active
-// const localPage = document.querySelectorAll(".page li");
-// localPage.forEach((li) => {
-//   li.addEventListener("click", (e) => {
-//     e.target.classList.remove("active")
-//   });
-// });
-
-//onclick toggle setting-box
-let gearSeting = document.getElementById("fa-gear");
-gearSeting.onclick = function () {
-  this.classList.toggle("fa-spin");
-  document.getElementById("setting-box").classList.toggle("open");
-};
-
-// switch  background random
-const randomBackgroundElement = document.querySelectorAll(".option-box button");
-randomBackgroundElement.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    e.target.parentElement.querySelectorAll(".active")
-    .forEach(element=>{
-      element.classList.remove("active")
-    });
-    e.target.classList.add("active")
-  });
-});
-// switch color
-
-const clorLi = document.querySelectorAll(".color-list li");
-
-clorLi.forEach((li) => {
-  li.addEventListener("click", (e) => {
-    // set color on root --main--color
-    document.documentElement.style.setProperty(
-      "--main--color",
-      e.target.dataset.color
-    );
-    // set color on local storage
-    localStorage.setItem("saving-color", e.target.dataset.color);
-    // remove all active class from all children
-    e.target.parentElement.querySelectorAll(".active").forEach((element) => {
-      element.classList.remove("active");
-    });
-    // add active to element clicked
-    e.target.classList.add("active");
-  });
-});
-
-// random landing back ground
-let myImages = document.getElementById("landing-page");
-
-const imagesArray = ["06.jpg", "07.jpg", "08.jpg"];
-
+const myImageArray = [
+  "01.jpg",
+  "02.jpg",
+  "03.jpg",
+  "04.jpg",
+  "05.jpg",
+  "06.jpg",
+  "07.jpg",
+  "08.jpg",
+];
 setInterval(() => {
-  let randomImager = Math.floor(Math.random() * imagesArray.length);
-  myImages.style.backgroundImage = `url('assets/${imagesArray[randomImager]}')`;
+  let randomImage = Math.floor(Math.random() * myImageArray.length);
+  landngPage.style.backgroundImage = `url('assets/${myImageArray[randomImage]}')`;
 }, 10000);
